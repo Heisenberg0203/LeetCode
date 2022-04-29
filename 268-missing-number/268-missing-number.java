98f4c6ba7c635478c28519ff686d60248e53bb68
Rushikesh Jachak <rushikesh0203@gmail.com>
2022-04-29 23:59:59 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (79.24%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -3,28 +3,12 @@ class Solution {
        // sorting - nlogn
         // hashset - n,n
         //9 6 4+9 2 3+9 5 7+9 0 1
-        
-        boolean isZero = false;
-        
+        int xor=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                nums[i]=nums.length;
-                isZero=true;
-                break;
-            }
+            xor=xor^i^nums[i];
         }
         
-        if(!isZero) return 0;
-        int length=nums.length;
-        for(int num:nums){
-            nums[Math.abs(num)%length] = -(Math.abs(nums[Math.abs(num)%length]));
-        }
-        
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>=0)  return i;
-        }
-        
-        return nums.length;
+        return xor^nums.length;
        
     }
 }
